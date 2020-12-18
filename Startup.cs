dc4e0ae1ccff6619cce91cb31da8dcb122a30ac3
Joe Berkley <joe.berkley@etchuk.com>
2020-12-18 14:31:44 +0000

    Add filters for getting select options from an array of content items

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Etch.OrchardCore.Lever.Api.Models.Dto;
 using Etch.OrchardCore.Lever.Api.Services;
 using Etch.OrchardCore.Lever.Drivers;
+using Etch.OrchardCore.Lever.Filters;
 using Etch.OrchardCore.Lever.Indexes;
 using Etch.OrchardCore.Lever.Models;
 using Etch.OrchardCore.Lever.Services;
@@ -13,6 +14,7 @@ using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.Data.Migration;
 using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.Indexing;
+using OrchardCore.Liquid;
 using OrchardCore.Modules;
 using OrchardCore.Navigation;
 using OrchardCore.Settings;
@@ -44,6 +46,11 @@ namespace Etch.OrchardCore.Lever
             services.AddScoped<IDisplayDriver<ISite>, LeverSettingsDisplayDriver>();
             services.AddScoped<IContentPartIndexHandler, LeverPostingPartIndexHandler>();
             services.AddScoped<IDataMigration, Migrations>();
+
+            services.AddLiquidFilter<CommitmentOptionsFilter>("lever_commitment_options");
+            services.AddLiquidFilter<LocationOptionsFilter>("lever_location_options");
+            services.AddLiquidFilter<TeamOptionsFilter>("lever_team_options");
+
             services.AddHttpClient();
         }
     }
---
+++ Utilities/StringUtils.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Etch.OrchardCore.Lever.Utilities
+{
+    public class StringUtils
+    {
+        public static string GetOptions(IList<string> items, string selectedValue)
+        {
+            StringBuilder bld = new StringBuilder();
+            bld.Append("<option value=\"\">All</option>\n");
+            for (int i = 0; i < items.Count; ++i)
+            {
+                var value = GetOptionValue(items[i]);
+
+                bld.Append(string.Format("<option value=\"{0}\"{1}>{2}</option>\n",
+                                value,
+                                !string.IsNullOrEmpty(selectedValue) && selectedValue == value ? " selected=\"selected\"" : "",
+                                items[i]));
+            }
+
+            return bld.ToString();
+        }
+
+        public static string GetOptionValue(string name)
+        {
+            HashSet<char> removeChars = new HashSet<char>("?&^$#@!()+-,:;<>’\'-_*");
+            StringBuilder result = new StringBuilder(name.Length);
+            foreach (char c in name.ToLower())
+            {
+                if (!removeChars.Contains(c))
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
