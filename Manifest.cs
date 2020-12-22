7b19d730c391a4f4f50b279a5997d0155ada49a9
Peter Keating <pete.keating@etchuk.com>
2020-12-22 17:30:31 +0000

    Bump to v0.2.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Lever job API with ability to apply for a job.",
     Name = "Lever",
-    Version = "0.1.0",
+    Version = "0.2.0",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "OrchardCore.BackgroundTasks", "OrchardCore.Forms" }
 )]
